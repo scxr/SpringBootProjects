23cac7b4a6b7d4b02c6aaf5be1b90e6eb46a8d50
scxr <cswilson@zoho.com>
2021-04-09 00:49:59 +0100

    Added some more thyme pages, not much of a thyme syntax fan, but we move

--- mystore/src/main/java/com/store/mystore/Product/ProductControllerRestless.java
+++ mystore/src/main/java/com/store/mystore/Product/ProductControllerRestless.java
@@ -3,6 +3,9 @@ package com.store.mystore.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +25,24 @@ public class ProductControllerRestless {
 
         model.addAttribute("products", products);
         return "productListing";
+    }
+
+    @GetMapping("register_product")
+    public String registerProductGet(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "productRegister";
+    }
+
+    @PostMapping("register_product")
+    public String registerProduct(Model model, @ModelAttribute("product") Product product){
+        try {
+            this.productServices.addProduct(product);
+            return "success";
+        } catch (Exception e) {
+            model.addAttribute("error", e);
+            return "fail";
+        }
+
     }
 }
